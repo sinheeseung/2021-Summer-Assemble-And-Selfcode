4c60a486b6a6de5df7c4c9f3d8ed91ed40202902
heeseung <tlsgmltmd726529@gmail.com>
2021-07-15 20:38:06 +0900

    검색어 입력하면 네이버 api를 통해 값을 읽어와 parsing

--- NaverSearch/app/src/main/java/com/example/naversearch/MainActivity.java
+++ NaverSearch/app/src/main/java/com/example/naversearch/MainActivity.java
@@ -9,11 +9,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private EditText EditText1;
     private Spinner ComboBox;
+    private Thread mThread;
+    private String inputLine;
+    private ArrayList<String> titleList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,12 +76,66 @@ public class MainActivity extends AppCompatActivity {
                     //검색버튼 클릭 => 검색어와 검색유형 읽어옴
                     String type = ComboBox.getSelectedItem().toString();
                     String query = EditText1.getText().toString();
-
+                    SearchNews(type,query);
+                    parsing();
                 case R.id.Button_Exit:
                     finish();
                     break;
             }
         }
     };
+    private void SearchNews(final String _category, final String searchWord) {
+        mThread = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        String clientId = ""; //애플리케이션 클라이언트 아이디값"
+                        String clientSecret = ""; //애플리케이션 클라이언트 시크릿값"
+                        try {
+                            String text = URLEncoder.encode(searchWord, "UTF-8");
+                            String apiURL = "https://openapi.naver.com/v1/search/" + _category + "?query=" + text; // json 결과
+                            URL url = new URL(apiURL);
+                            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                            con.setRequestMethod("GET");
+                            con.setRequestProperty("X-Naver-Client-Id", clientId);
+                            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+                            int responseCode = con.getResponseCode();
+                            BufferedReader br;
+                            if (responseCode == 200) { // 정상 호출
+                                br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+                            } else {  // 에러 발생
+                                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                            }
+                            inputLine = br.readLine();
+                            br.close();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        mThread.start();
+    }
+
+    /*검색 결과 전처리*/
+    private void parsing() {
+        String title;
+        String link;
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject(inputLine);
+            JSONArray jsonArray = jsonObject.getJSONArray("items");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject item = jsonArray.getJSONObject(i);
+                title = item.getString("title");
+                link = item.getString("link");
+                title = title.replace("<(/)?([a-zA-Z]*)(\\s[a-zA-Z]*=[^>]*)?(\\s)*(/)?>", "").replace("&quot;", "")
+                        .replace("&lt;", "").replace("&gt;", "").replace("&amp;", "").replace("=", "");
+                System.out.println(title);
+                titleList.add((title));;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
